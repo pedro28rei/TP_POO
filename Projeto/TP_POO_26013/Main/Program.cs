bdf2584e158f3d11da03db399d2a00eb4708c092
pedro28rei <a26013@alunos.ipca.pt>
2023-11-14 13:10:31 +0000

    Mudanças na Solução

---
+++ Projeto/TP_POO_26013/Main/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+* Descrição: Criação do Projeto
+* Nome: Pedro Rei
+* Contacto: pedro28rei@gmail.com
+* Data: 1/11/2023
+* LESI - POO
+* 
+* 
+*/
+
+
+using System;
+using Pessoas;
+
+namespace Main
+{
+    /// <summary>
+    /// A classe que vai ser a main onde se encontra o programa
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projeto/TP_POO_26013/Pessoas/pessoa.cs
@@ -0,0 +1,108 @@
+﻿/*
+* Descrição: Elaboração da classe pessoa
+* Nome: Pedro Rei
+* Contacto: pedro28rei@gmail.com
+* Data: 05/11/2023
+* LESI - POO
+* 
+* 
+*/
+
+namespace Pessoas
+{
+    public class pessoa
+    {
+        #region ATRIBUTOS
+
+        private string nome;
+        private int idade;
+        private DateOnly dataNascimento;
+
+
+        #endregion
+
+
+        #region METODOS
+
+
+        #region CONSTRUTORES
+
+        /// <summary>
+        /// Construtor por omissão que cria uma pessoa,sem parâmetros definidos 
+        /// </summary>
+        public pessoa()
+        {
+            nome = "";
+            idade = 0;
+            dataNascimento = new DateOnly(0, 0, 0);
+        }
+
+
+        /// <summary>
+        /// Construtor por omissão que cria uma pessoa, com parâmetros definidos
+        /// </summary>
+        /// <param name="nome"></param>
+        /// <param name="idade"></param>
+        /// <param name="dataNascimento"></param>
+        public pessoa(string nome, int idade, DateOnly dataNascimento)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.dataNascimento = dataNascimento;
+        }
+
+        #endregion
+
+
+        #region PRORIEDADES
+
+        /// <summary>
+        /// Propriedade da idade de uma pessoa
+        /// </summary>
+        public int Idade
+        {
+            set { idade = value; }
+            get { return idade; }
+        }
+
+        /// <summary>
+        /// Propriedade do nome de uma pessoa
+        /// </summary>
+        public string Nome
+        {
+            set { nome = value; }
+            get { return nome; }
+        }
+        /// <summary>
+        /// Propriedade da dataNascimento de uma pessoa
+        /// </summary>
+
+        public DateOnly DataNascimento
+        {
+            set { dataNascimento = value; }
+            get { return dataNascimento; }
+        }
+
+        #endregion
+
+
+        #region ATRIBUTOS
+        #endregion
+
+
+        #region OPERADORES
+        #endregion
+
+
+        #region OVERRIDES
+        #endregion
+
+
+        #region DESTRUIDORES
+        #endregion
+
+        #endregion
+
+    }
+
+}
